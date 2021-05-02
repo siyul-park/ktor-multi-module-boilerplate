4e1417f054e13c07816987d9ef37c375a2beb69e
siyual-park <kdpark0723@gmail.com>
2021-05-02 13:15:33 +0900

    fix error

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,14 +80,6 @@ allprojects {
                     project.name
                 }
             )
-            properties.setProperty(
-                "description",
-                if (project.description == "unspecified") {
-                    rootProject.description
-                } else {
-                    project.description
-                }
-            )
 
             properties.store(propertiesFile.writer(), null)
         }
