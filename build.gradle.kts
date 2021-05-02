453e9eb6e947217fd2683097b08d9ca6e6b45a08
Ara Park <siyual.bak@gmail.com>
2021-05-02 11:17:29 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+val kotlin_version: String by project
+
+buildscript {
+    val klint_version: String by project
+
+    repositories {
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        jcenter()
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:$klint_version")
+    }
+}
+
+plugins {
+    kotlin("jvm")
+    id("org.jlleitschuh.gradle.ktlint")
+}
+
+group = "io.github.siyual_park"
+version = "0.0.1-SNAPSHOT"
+
+allprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenLocal()
+        jcenter()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib"))
+    }
+}
