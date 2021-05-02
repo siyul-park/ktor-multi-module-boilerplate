c3593a6d301f38c4ac767ef3cf66de1eab5b2e0d
siyual-park <kdpark0723@gmail.com>
2021-05-02 11:30:00 +0900

    create

---
+++ application/api/src/main/kotlin/io/github/siyual_park/application/api/Application.kt
@@ -0,0 +1,17 @@
+package io.github.siyual_park.application.api
+
+import io.github.siyual_park.application.api.routes.getRootRoutes
+import io.github.siyual_park.application.api.routes.pingRoutes
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.routing.Routing
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    install(Routing) {
+        getRootRoutes()
+        pingRoutes()
+    }
+}
